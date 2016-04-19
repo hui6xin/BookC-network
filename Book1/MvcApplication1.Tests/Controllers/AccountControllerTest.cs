a0688feeae1c6a04626d890a238b15b6984f646c
hui6xin <hui6xin@163.com>
2016-04-19 17:24:31 +0800

    1

---
+++ Book1/MvcApplication1.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcApplication1;
+using MvcApplication1.Controllers;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Tests.Controllers
+{
+
+    [TestClass]
+    public class AccountControllerTest
+    {
+
+        [TestMethod]
+        public void ChangePassword_Get_ReturnsView()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+
+            // 操作
+            ActionResult result = controller.ChangePassword();
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.AreEqual(10, ((ViewResult)result).ViewData["PasswordLength"]);
+        }
+
+        [TestMethod]
+        public void ChangePassword_Post_ReturnsRedirectOnSuccess()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            ChangePasswordModel model = new ChangePasswordModel()
+            {
+                OldPassword = "goodOldPassword",
+                NewPassword = "goodNewPassword",
+                ConfirmPassword = "goodNewPassword"
+            };
+
+            // 操作
+            ActionResult result = controller.ChangePassword(model);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.AreEqual("ChangePasswordSuccess", redirectResult.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void ChangePassword_Post_ReturnsViewIfChangePasswordFails()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            ChangePasswordModel model = new ChangePasswordModel()
+            {
+                OldPassword = "goodOldPassword",
+                NewPassword = "badNewPassword",
+                ConfirmPassword = "badNewPassword"
+            };
+
+            // 操作
+            ActionResult result = controller.ChangePassword(model);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(model, viewResult.ViewData.Model);
+            Assert.AreEqual("当前密码不正确或新密码无效。", controller.ModelState[""].Errors[0].ErrorMessage);
+            Assert.AreEqual(10, viewResult.ViewData["PasswordLength"]);
+        }
+
+        [TestMethod]
+        public void ChangePassword_Post_ReturnsViewIfModelStateIsInvalid()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            ChangePasswordModel model = new ChangePasswordModel()
+            {
+                OldPassword = "goodOldPassword",
+                NewPassword = "goodNewPassword",
+                ConfirmPassword = "goodNewPassword"
+            };
+            controller.ModelState.AddModelError("", "虚拟错误消息。");
+
+            // 操作
+            ActionResult result = controller.ChangePassword(model);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(model, viewResult.ViewData.Model);
+            Assert.AreEqual(10, viewResult.ViewData["PasswordLength"]);
+        }
+
+        [TestMethod]
+        public void ChangePasswordSuccess_ReturnsView()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+
+            // 操作
+            ActionResult result = controller.ChangePasswordSuccess();
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void LogOff_LogsOutAndRedirects()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+
+            // 操作
+            ActionResult result = controller.LogOff();
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.AreEqual("Home", redirectResult.RouteValues["controller"]);
+            Assert.AreEqual("Index", redirectResult.RouteValues["action"]);
+            Assert.IsTrue(((MockFormsAuthenticationService)controller.FormsService).SignOut_WasCalled);
+        }
+
+        [TestMethod]
+        public void LogOn_Get_ReturnsView()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+
+            // 操作
+            ActionResult result = controller.LogOn();
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void LogOn_Post_ReturnsRedirectOnSuccess_WithoutReturnUrl()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "goodPassword",
+                RememberMe = false
+            };
+
+            // 操作
+            ActionResult result = controller.LogOn(model, null);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.AreEqual("Home", redirectResult.RouteValues["controller"]);
+            Assert.AreEqual("Index", redirectResult.RouteValues["action"]);
+            Assert.IsTrue(((MockFormsAuthenticationService)controller.FormsService).SignIn_WasCalled);
+        }
+
+        [TestMethod]
+        public void LogOn_Post_ReturnsRedirectOnSuccess_WithReturnUrl()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "goodPassword",
+                RememberMe = false
+            };
+
+            // 操作
+            ActionResult result = controller.LogOn(model, "/someUrl");
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            RedirectResult redirectResult = (RedirectResult)result;
+            Assert.AreEqual("/someUrl", redirectResult.Url);
+            Assert.IsTrue(((MockFormsAuthenticationService)controller.FormsService).SignIn_WasCalled);
+        }
+
+        [TestMethod]
+        public void LogOn_Post_ReturnsViewIfModelStateIsInvalid()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "goodPassword",
+                RememberMe = false
+            };
+            controller.ModelState.AddModelError("", "虚拟错误消息。");
+
+            // 操作
+            ActionResult result = controller.LogOn(model, null);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(model, viewResult.ViewData.Model);
+        }
+
+        [TestMethod]
+        public void LogOn_Post_ReturnsViewIfValidateUserFails()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "badPassword",
+                RememberMe = false
+            };
+
+            // 操作
+            ActionResult result = controller.LogOn(model, null);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(model, viewResult.ViewData.Model);
+            Assert.AreEqual("提供的用户名或密码不正确。", controller.ModelState[""].Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Register_Get_ReturnsView()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+
+            // 操作
+            ActionResult result = controller.Register();
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.AreEqual(10, ((ViewResult)result).ViewData["PasswordLength"]);
+        }
+
+        [TestMethod]
+        public void Register_Post_ReturnsRedirectOnSuccess()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            RegisterModel model = new RegisterModel()
+            {
+                UserName = "someUser",
+                Email = "goodEmail",
+                Password = "goodPassword",
+                ConfirmPassword = "goodPassword"
+            };
+
+            // 操作
+            ActionResult result = controller.Register(model);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(RedirectToRouteResult));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.AreEqual("Home", redirectResult.RouteValues["controller"]);
+            Assert.AreEqual("Index", redirectResult.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void Register_Post_ReturnsViewIfRegistrationFails()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            RegisterModel model = new RegisterModel()
+            {
+                UserName = "duplicateUser",
+                Email = "goodEmail",
+                Password = "goodPassword",
+                ConfirmPassword = "goodPassword"
+            };
+
+            // 操作
+            ActionResult result = controller.Register(model);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(model, viewResult.ViewData.Model);
+            Assert.AreEqual("用户名已存在。请另输入一个用户名。", controller.ModelState[""].Errors[0].ErrorMessage);
+            Assert.AreEqual(10, viewResult.ViewData["PasswordLength"]);
+        }
+
+        [TestMethod]
+        public void Register_Post_ReturnsViewIfModelStateIsInvalid()
+        {
+            // 排列
+            AccountController controller = GetAccountController();
+            RegisterModel model = new RegisterModel()
+            {
+                UserName = "someUser",
+                Email = "goodEmail",
+                Password = "goodPassword",
+                ConfirmPassword = "goodPassword"
+            };
+            controller.ModelState.AddModelError("", "虚拟错误消息。");
+
+            // 操作
+            ActionResult result = controller.Register(model);
+
+            // 断言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.AreEqual(model, viewResult.ViewData.Model);
+            Assert.AreEqual(10, viewResult.ViewData["PasswordLength"]);
+        }
+
+        private static AccountController GetAccountController()
+        {
+            AccountController controller = new AccountController()
+            {
+                FormsService = new MockFormsAuthenticationService(),
+                MembershipService = new MockMembershipService()
+            };
+            controller.ControllerContext = new ControllerContext()
+            {
+                Controller = controller,
+                RequestContext = new RequestContext(new MockHttpContext(), new RouteData())
+            };
+            return controller;
+        }
+
+        private class MockFormsAuthenticationService : IFormsAuthenticationService
+        {
+            public bool SignIn_WasCalled;
+            public bool SignOut_WasCalled;
+
+            public void SignIn(string userName, bool createPersistentCookie)
+            {
+                // 验证参数是否为预期参数
+                Assert.AreEqual("someUser", userName);
+                Assert.IsFalse(createPersistentCookie);
+
+                SignIn_WasCalled = true;
+            }
+
+            public void SignOut()
+            {
+                SignOut_WasCalled = true;
+            }
+        }
+
+        private class MockHttpContext : HttpContextBase
+        {
+            private readonly IPrincipal _user = new GenericPrincipal(new GenericIdentity("someUser"), null /* roles */);
+
+            public override IPrincipal User
+            {
+                get
+                {
+                    return _user;
+                }
+                set
+                {
+                    base.User = value;
+                }
+            }
+        }
+
+        private class MockMembershipService : IMembershipService
+        {
+            public int MinPasswordLength
+            {
+                get { return 10; }
+            }
+
+            public bool ValidateUser(string userName, string password)
+            {
+                return (userName == "someUser" && password == "goodPassword");
+            }
+
+            public MembershipCreateStatus CreateUser(string userName, string password, string email)
+            {
+                if (userName == "duplicateUser")
+                {
+                    return MembershipCreateStatus.DuplicateUserName;
+                }
+
+                // 验证值是否为预期值
+                Assert.AreEqual("goodPassword", password);
+                Assert.AreEqual("goodEmail", email);
+
+                return MembershipCreateStatus.Success;
+            }
+
+            public bool ChangePassword(string userName, string oldPassword, string newPassword)
+            {
+                return (userName == "someUser" && oldPassword == "goodOldPassword" && newPassword == "goodNewPassword");
+            }
+        }
+
+    }
+}
---
+++ Book1/MvcApplication1.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcApplication1;
+using MvcApplication1.Controllers;
+
+namespace MvcApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // 排列
+            HomeController controller = new HomeController();
+
+            // 操作
+            ViewResult result = controller.Index() as ViewResult;
+
+            // 断言
+            ViewDataDictionary viewData = result.ViewData;
+            Assert.AreEqual("欢迎使用 ASP.NET MVC!", viewData["Message"]);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // 排列
+            HomeController controller = new HomeController();
+
+            // 操作
+            ViewResult result = controller.About() as ViewResult;
+
+            // 断言
+            Assert.IsNotNull(result);
+        }
+    }
+}
